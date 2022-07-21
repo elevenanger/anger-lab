01a90d1a632b0db88c9ce9bfecef8f11c344292e
elevenanger <550356416@qq.com>
2022-07-21 18:43:44 +0800

    spring mvc

---
+++ spring/mvc/src/main/java/mvc/controller/HomeController.java
@@ -0,0 +1,31 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * author : liuanglin
+ * date : 2022/7/21 08:20
+ * description : 主页面 controller
+ */
+@Controller
+public class HomeController {
+
+    /*
+    Model 存储 controller 发送给视图的数据
+     */
+    @RequestMapping("/home")
+    public String home(Model page,
+                       @RequestParam(required = false) String name,
+                       @RequestParam(required = false) String color) {
+        /*
+        添加需要 controller 发送给 view 的数据
+         */
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
+        return "home";
+    }
+
+}
