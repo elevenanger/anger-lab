64b59747e527c7a98449b38ad9d84aad02c86aa7
elevenanger <550356416@qq.com>
2022-12-13 21:02:03 +0800

    sharing objects

---
+++ performance/concurrency/sharingobjects/src/main/java/cn/anger/visibility/SynchronizedInteger.java
@@ -0,0 +1,17 @@
+package cn.anger.visibility;
+
+import cn.anger.annotation.GuardedBy;
+import cn.anger.annotation.ThreadSafe;
+
+/**
+ * @author : anger
+ * 对于 get 和 set 方法
+ * 都使用 synchronized 关键字
+ * 才能避免多线程环境下过时数据的问题
+ */
+@ThreadSafe
+public class SynchronizedInteger {
+    @GuardedBy("this") private int value;
+    public synchronized int getValue() { return value; }
+    public synchronized void setValue(int value) { this.value = value; }
+}
